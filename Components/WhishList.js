e38ac4a15c7ff6ac19e46d0e570cd01ddde332cf
boureimatapily <boureimatapily1@gmail.com>
2020-04-30 19:00:10 +0000

    ADD Checkout, Item, WhisList Components

---
+++ Components/WhishList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+export class WhishList extends Component {
+  render() {
+    return (
+      <View>
+        <Text> hello</Text>
+      </View>
+    )
+  }
+}
+
+const style = StyleSheet.create({})
+
+export default WhishList
