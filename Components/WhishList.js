1cca39ba2af2d9e0a8c4daf8447630602061bf5f
boureimatapily <boureimatapily1@gmail.com>
2020-05-01 01:09:40 +0000

    MOD space

--- Components/WhishList.js
+++ Components/WhishList.js
@@ -1,16 +1,97 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, ScrollView, FlatList, TouchableOpacity } from 'react-native'
+import Item from './Item'
+import { Feather } from '@expo/vector-icons'
 
 export class WhishList extends Component {
-  render() {
-    return (
-      <View>
-        <Text> hello</Text>
-      </View>
-    )
-  }
+
+    List = [
+        { id: "1", name: "Low classic", detail: "Reachable choes Nike", price: "$2450", image: require('../assets/shoes.jpg') },
+        { id: "2", name: "Addidas", detail: "Unreachable choes Nike", price: "$3560", image: require('../assets/shoes1.jpg') },
+        { id: "3", name: "Roadster", detail: "Expanding choes Nike", price: "$4892", image: require('../assets/shoes2.jpg') },
+        { id: "4", name: "Nike", detail: "Knight Rider choes Nike", price: "$2598", image: require('../assets/shoes3.jpg') },
+        
+    ]
+
+
+    render() {
+        return (
+            <ScrollView style={styles.container}>
+                <View style={styles.headerContainer}>
+                    <TouchableOpacity>
+                        <Text style={styles.headerShopes} >Shopes</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={styles.headerWhislist}>Whish List</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity>
+                        <Feather name="shopping-cart" size={20} />
+                    </TouchableOpacity>
+                </View>
+                <FlatList
+                    numColumns={2}
+                    contentContainerStyle={{marginBottom:30}}
+                    showsVerticalScrollIndicator={false}
+                    data={this.List}
+                    renderItem={({ item }) => {
+                        return <Item name={item.name} detail={item.detail}
+                            price={item.price} img={item.image} />
+                    }}
+                    keyExtractor={item => { item.id }}
+                />
+
+                <View style={styles.footerIcons} >
+                        <TouchableOpacity>
+                        <Feather name="menu" size={20} />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Feather name="truck" size={20} />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                             <Feather name="search" size={20} />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                              <Feather name="shopping-bag" size={20}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Feather name="bell" size={20} />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Feather name="user" size={20} />
+                        </TouchableOpacity>
+                </View>
+
+            </ScrollView>
+
+        )
+    }
 }
 
-const style = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        marginVertical: 5,
+    },
+    headerContainer: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        marginBottom: 50
+    },
+    headerShopes: {
+        fontSize: 16,
+        color: "red",
+        fontWeight: "bold"
+    },
+    headerWhislist: {
+        fontWeight: "bold",
+    },
+    footerIcons:{
+       flexDirection:"row",
+       justifyContent:"space-evenly",
+        marginTop:50
+    },
+
+
+})
 
 export default WhishList
